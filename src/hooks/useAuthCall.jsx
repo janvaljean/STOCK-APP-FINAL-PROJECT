6e19ca70df3a6cd7ff9f4d85e118bdd1c55d9a88
janvaljean <kurtuldu@gmx.de>
2023-03-24 13:54:36 +0100

    register login logout done

--- src/hooks/useAuthCall.jsx
+++ src/hooks/useAuthCall.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
+import { fetchFail, fetchStart, loginSuccess, logoutSuccess, registerSuccess } from "../features/authSlice";
 
 const useAuthCall = () => {
     const dispatch =useDispatch()
@@ -26,7 +26,38 @@ const useAuthCall = () => {
       console.log(error)
     }
   }
-      return ( login )
+    const register = async (userInfo) => {
+    const BASE_URL = "https://14107.fullstack.clarusway.com/"
+    dispatch(fetchStart())
+    try {
+      const { data } = await axios.post(
+        `${BASE_URL}account/register/`,
+        userInfo
+        )
+        console.log(data)
+      dispatch(registerSuccess(data))
+      navigate("/stock")
+    } catch (error) {
+      dispatch(fetchFail())
+      console.log(error)
+    }
+  }
+  const logout = async () => {
+    const BASE_URL = "https://14107.fullstack.clarusway.com/"
+    dispatch(fetchStart())
+    try {
+      const { data } = await axios.post(
+        `${BASE_URL}account/auth/logout/`,
+        )
+      dispatch(logoutSuccess(data))
+      navigate("/")
+
+    } catch (error) {
+      dispatch(fetchFail())
+      
+    }
+  }
+      return ( login , register, logout)
 }
 
 export default useAuthCall
