0c5aa1afc4a7c1e7911c24877cf1662ef384269c
janvaljean <kurtuldu@gmx.de>
2023-03-27 01:35:24 +0200

    Dashboard Router done

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,13 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Purchases from "../pages/Purchases";
+import Home from "../pages/Home";
+import Firms from "../pages/Firms";
+import Brands from "../pages/Brands";
+import Products from "../pages/Products";
+import Sales from "../pages/Sales";
+
 
 const AppRouter = () => {
   return (
@@ -12,7 +19,14 @@ const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            <Route path="firms" element={<Firms />}/>
+            <Route index element={<Home />}/>
+            <Route path="products" element={<Products />}/>
+            <Route path="purchases" element={<Purchases />}/>
+            <Route path="brands" element={<Brands />}/>
+            <Route path="sales" element={<Sales />}/>
+          </Route>
         </Route>
       </Routes>
     </Router>
