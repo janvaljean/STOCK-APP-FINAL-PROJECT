e8c21d5b879b643f3286fa54e64c5afc717ce61c
janvaljean <kurtuldu@gmx.de>
2023-03-31 03:40:05 +0200

    products table mui fixed

---
+++ src/components/modals/ProductModal.jsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import { modelStyle } from '../../styles/globalStyle';
+import  TextField  from '@mui/material/TextField';
+import useStockCall from '../../hooks/useStockCall';
+
+
+
+export default function ProductModal({handleClose, open,info, setInfo, initialInfo }) {
+   
+    
+    const {postStockData, putStockData} = useStockCall()
+
+    const handleChange = (e) => {
+        
+        const{name,value} = e.target
+        setInfo({...info, [name] : value})
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(info.id ){putStockData("product",info)}
+        else{ postStockData("product", info)}
+        handleClose()
+        setInfo(initialInfo)
+    }
+  
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={() => {
+            handleClose()
+            setInfo(initialInfo)
+        }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modelStyle}>
+          <Box onSubmit={handleSubmit} component="form" sx={{ display: "flex", flexDirection: "column", gap: 2}}>
+            <TextField
+            label="Firm Name"
+            name="name"
+            id="name"
+            type="text"
+            variant="outlined"
+            value={info?.name}
+            onChange={handleChange}
+            
+            />
+            <TextField
+            label="Phone Number"
+            name="phone"
+            id="phone"
+            type="text"
+            variant="outlined"
+            value={info?.phone}
+            onChange={handleChange}
+            
+            />
+            <TextField
+            label="Address"
+            name="address"
+            id="address"
+            type="text"
+            variant="outlined"
+            value={info?.address}
+            onChange={handleChange}
+            
+            />
+            <TextField
+            label="Image"
+            name="image"
+            id="image"
+            type="text"
+            variant="outlined"
+            value={info?.image}
+            onChange={handleChange}
+            />
+            <Button
+          variant="contained"
+          type="submit"
+        >
+          Submit
+        </Button>
+            </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,106 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import useStockCall from '../hooks/useStockCall';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Button, Grid, Typography } from '@mui/material';
+import ProductModal from '../components/modals/ProductModal';
+
+
 
 const Products = () => {
+  
+const columns = [
+  { field: 'id', headerName: '#',maxWidth: 70, minWidth: 40, flex: 1,headerAlign: 'center',
+    align: 'center',},
+  {
+    field: 'category',
+    headerName: 'Category',
+    headerAlign: 'center',
+    align: 'center',
+    minWidth: 150,
+    flex: 3,
+  },
+  {
+    field: 'brand',
+    headerName: 'Brand',
+    headerAlign: 'center',
+    align: 'center',
+    minWidth: 150,
+    flex: 2,
+  },
+  {
+    field: 'name',
+    headerName: 'Name',
+    headerAlign: 'center',
+    align: 'center',
+    type: 'number',
+    minWidth: 100,
+    flex: 2,
+  },
+  {
+    field: 'stock',
+    headerName: 'Stock',
+    headerAlign: 'center',
+    align: 'center',
+    minWidth: 50,
+    flex: 0.7,
+  },
+   {
+    field: 'actions',
+    headerName: 'Actions',
+    headerAlign: 'center',
+    align: 'center',
+    type: 'number',
+    maxWidth: 80,
+    flex:1
+  },
+];
+    const initialInfo = {
+          name: "",
+          phone: "",
+          address: "",
+          image: "",
+    }
+
+   const { getStockData } = useStockCall()
+   const {products} = useSelector((state) => state.stock)
+   const [open, setOpen] = React.useState(false);
+   const handleOpen = () => setOpen(true);
+   const handleClose = () => setOpen(false);
+   const [info, setInfo] = useState(initialInfo)
+
+    useEffect(() => {
+      getStockData("products")
+  }, [])
+    console.log(products)
   return (
     <div>
-      
-    </div>
-  )
-}
 
+    <Typography variant="h4" color="error">Products</Typography>
+      <Button variant="contained" onClick={handleOpen}>New Firms</Button>
+      <ProductModal handleClose={handleClose} handleOpen={handleOpen} setInfo={setInfo} info={info} initialInfo={initialInfo}/>
+  
+    <Box sx={{ width: '100%' }}>
+      <DataGrid
+        autoHeight
+        rows={products}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        disableRowSelectionOnClick
+      />
+    </Box>
+
+    </div>
+
+  )}   
 export default Products
